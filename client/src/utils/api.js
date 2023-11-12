a170bfe9088810d610e856bde00ef71c43c8e733
Sabari-A <sabari.2k02@gmail.com>
2023-11-12 17:55:32 +0530

    base url updated

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://real-estate-project-olive.vercel.app/api",
+  baseURL: "https://real-estate-project-pied.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
